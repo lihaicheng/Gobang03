7454537d971d5170c0112f51dfc0c796d7fd3e2b
bzsome <chao@DESKTOP-CHAO>
2018-07-28 22:18:32 +0800

    增加数据库文件

---
+++ server/src/main/resources/gobang03.sql
@@ -0,0 +1,45 @@
+﻿# Host: localhost  (Version: 5.5.53)
+# Date: 2018-07-28 11:43:40
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "user_roles"
+#
+
+DROP TABLE IF EXISTS `user_roles`;
+CREATE TABLE `user_roles` (
+  `username` varchar(255) NOT NULL,
+  `role_name` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user_roles"
+#
+
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+
+#
+# Structure for table "users"
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL COMMENT '用户名',
+  `password` varchar(255) NOT NULL COMMENT '用户密码',
+  `email` varchar(255) DEFAULT NULL,
+  `sign` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "users"
+#
+
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'chao','ed3c6f5acc8f9a9065cf57928f73c126','q2323@qq.com','123未确认'),(3,'admins','a4f20870d8da975dca54fdbe731aad18',NULL,NULL),(5,'adminss','4261aebd6e06de976322b6932d3480f3','333333@qq.com',NULL),(6,'admin2','7f132dda17417466de0e73841ce00bbf','','124234234231');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
